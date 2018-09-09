b51a458b40ee212cb9b06dfa6627e1c16345056c
Królik Uszasty <bunny@awril.pl>
2018-09-09 15:10:12 +0200

    Rotating masses excluded of load coefficient

--- McZapkie/Mover.cpp
+++ McZapkie/Mover.cpp
@@ -3463,7 +3463,7 @@ void TMoverParameters::UpdatePipePressure(double dt)
                 Hamulec->PLC( MaxBrakePress[ LoadFlag ] );
             else
                 //(Hamulec as TWest).PLC(TotalMass);
-                Hamulec->PLC( TotalMass );
+                Hamulec->PLC( TotalMass-Mred );
             break;
         }
 
@@ -3493,7 +3493,7 @@ void TMoverParameters::UpdatePipePressure(double dt)
                 if( MBPM < 2 )
                     Hamulec->PLC( MaxBrakePress[ LoadFlag ] );
                 else
-                    Hamulec->PLC( TotalMass );
+                    Hamulec->PLC( TotalMass-Mred );
             break;
         }
 
@@ -3507,7 +3507,7 @@ void TMoverParameters::UpdatePipePressure(double dt)
 
         case TBrakeValve::EP2:
         {
-            Hamulec->PLC( TotalMass );
+            Hamulec->PLC( TotalMass-Mred );
             break;
         }
         case TBrakeValve::ESt3AL2:
@@ -3520,7 +3520,7 @@ void TMoverParameters::UpdatePipePressure(double dt)
                 Hamulec->PLC( MaxBrakePress[ LoadFlag ] );
             else
                 //(Hamulec as TNESt3).PLC(TotalMass);
-                Hamulec->PLC( TotalMass );
+                Hamulec->PLC( TotalMass-Mred );
             LocBrakePress = LocHandle->GetCP();
             //(Hamulec as TNESt3).SetLBP(LocBrakePress);
             Hamulec->SetLBP( LocBrakePress );
@@ -3536,7 +3536,7 @@ void TMoverParameters::UpdatePipePressure(double dt)
                 Hamulec->PLC( MaxBrakePress[ LoadFlag ] );
             else
                 //(Hamulec as TKE).PLC(TotalMass);
-                Hamulec->PLC( TotalMass );
+                Hamulec->PLC( TotalMass-Mred );
             break;
         }
         default:
